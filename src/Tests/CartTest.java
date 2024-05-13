befc693aefa7ebcc017cc9c95e779be1c08d274b
JamesCutter-dev <jameslbarnes@live.co.uk>
2024-05-14 00:08:45 +0100

    Version 1.1
    Added 4 junit tests
    
    ProductTest
    - tests creating products
    - tests applying discounts
    - tests price after discount
    
    ProductManagerTest
    - Tests adding a product
    - Tests viewing products
    - Tests updating a product
    - Tests removing a product
    
    CartItem Test
    - Tests creation of a cart item
    - Test total price calculation
    
    Cart Class
    - Tests adding an item
    - Test removing an item
    - Test applying discount to the cart
    - Tests calculating total
    - Tests processing payment
    - Tests genrerating receipts

---
+++ src/Tests/CartTest.java
@@ -0,0 +1,61 @@
+package Tests;
+
+import SecureEpos.Cart;
+import SecureEpos.Product;
+import SecureEpos.CartItem;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CartTest {
+
+    @Test
+    void testAddItem() {
+        Cart cart = new Cart();
+        Product product = new Product(1, "Test Product", 100.0, 10);
+        cart.addItem(product, 5);
+        assertEquals(1, cart.getItems().size());
+    }
+
+    @Test
+    void testRemoveItem() {
+        Cart cart = new Cart();
+        Product product = new Product(1, "Test Product", 100.0, 10);
+        cart.addItem(product, 5);
+        cart.removeItem(product);
+        assertEquals(0, cart.getItems().size());
+    }
+    @Test
+    void testApplyDiscount() {
+        Cart cart = new Cart();
+        cart.applyDiscount(10.0);
+        assertEquals(10, cart.getDiscount());
+    }
+
+    @Test
+    void testCalculateTotalPrice() {
+        Cart cart = new Cart();
+        Product product = new Product(1, "Test Product", 100.0, 10);
+        cart.addItem(product, 5);
+        assertEquals(500.0, cart.calculateTotal());
+    }
+
+    @Test
+    void testProcessPayment() {
+        Cart cart = new Cart();
+        Product product = new Product(1, "Test Product", 100.0, 10);
+        cart.addItem(product, 5);
+        cart.processPayment(500.0);
+        assertEquals(0, cart.processPayment(500));
+    }
+
+    @Test
+    void testGenerateReceipt() {
+        Cart cart = new Cart();
+        Product product = new Product(1, "Test Product", 100.0, 10);
+        cart.addItem(product, 5);
+        cart.processPayment(500.0);
+        cart.generateReceipt(); // manually verify receipt output
+    }
+
+
+}
---
+++ src/Tests/ProductTest.java
@@ -0,0 +1,32 @@
+package Tests;
+import SecureEpos.Product;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ProductTest {
+
+    @Test
+    void testProductCreation() {
+        Product product = new Product(1, "Test Product", 100.0, 10);
+        assertEquals(1, product.getID());
+        assertEquals("Test Product", product.getName());
+        assertEquals(100.0, product.getPrice());
+        assertEquals(10, product.getQuantity());
+        assertEquals(0.0, product.getDiscount());
+    }
+
+    @Test
+    void testApplyingDiscount() {
+        Product product = new Product(1, "Test Product", 100.0, 10);
+        product.setDiscount(10.0);
+        assertEquals(90.0, product.getPriceAfterDiscount());
+    }
+
+    @Test
+    void testPriceAfterDiscount() {
+        Product product = new Product(1, "Test Product", 100.0, 10);
+        product.setDiscount(10.0);
+        assertEquals(90.0, product.getPriceAfterDiscount());
+    }
+
+}
